1085620f71c259cf60c37f0ebbb69d4f4b503925
Fedor <fazzysmart@yandex.ru>
2015-03-11 01:00:22 +0300

    + новые изменения

--- plugin_filter_hough/plugin_filter_hough/filter_hough.h
+++ plugin_filter_hough/plugin_filter_hough/filter_hough.h
@@ -2,11 +2,13 @@
 #define FILTER_HOUGH
 
 
-#include "avisynth.h
-class FilterHough
-{
-public:
-	FilterHough(void);
+#include <windows.h>
+#include "avisynth.h"
 
+class InvertNeg : public GenericVideoFilter {
+public:
+   InvertNeg(PClip _child, IScriptEnvironment* env);
+   PVideoFrame __stdcall GetFrame(int n, IScriptEnvironment* env);
 };
+
 #endif
