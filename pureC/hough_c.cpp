e43de56a30b81738fe7ad8c69d2ceca585a82809
Fedor <fazzysmart@yandex.ru>
2015-03-23 07:56:16 +0300

    + modify

--- pureC/hough_c.cpp
+++ pureC/hough_c.cpp
@@ -1,5 +1,6 @@
 #include <string.h>
 #include "hough_c.h"
+//#include "stdio.h"
 //! преобразование Хафа
 //! image - указатель на изображение
 //! r0 - длина(в пикселях) отрезков по которым идет интерполирование,
@@ -64,7 +65,7 @@ void calcDiff(uchar* di, uchar* index,int col, int row)
     }
 }
 //! расчет
-void calcArray(uchar *mA,uchar *mB,int col,int row,uchar *v1,uchar *v2,
+void calcArray(uchar *mA,uchar *mB,int col,int row,int *v1,int *v2,
                uchar *index,uchar* di,int colIndex,int rowIndex)
 {
     static uchar vv1[ROW_MAX];
@@ -76,26 +77,26 @@ void calcArray(uchar *mA,uchar *mB,int col,int row,uchar *v1,uchar *v2,
     for(int l=0;l<colIndex;l++)
     {
         //! индексы
-        int r2=(l+1)<<1;
+        int r2=((l+1)<<1)-1;
         int r1=r2-1;
-        for(int i=0;i<colIndex;i++)
+        for(int i=0;i<row;i++)
         {
-            vv1[i]=v1[i]+index[0*rowIndex + l]%row;
-            vv3[i]=vv1[i]+di[0*rowIndex + l]%row;
+            vv1[i]=v1[i]+index[0*colIndex + l]%row;
+            vv3[i]=vv1[i]+di[0*colIndex + l]%row;
         }
-        for(int i=0;i<rowIndex;i++)
+        for(int i=0;i<col;i++)
         {
-            vv2[i]=v2[i]+index[1*rowIndex + l]%col;
-            vv4[i]=vv2[i]+index[1*rowIndex + l]%col;
+            vv2[i]=v2[i]+index[1*colIndex + l]%col;
+            vv4[i]=vv2[i]+index[1*colIndex + l]%col;
         }
         for(int i=0;i<row;i++)
         {
             for(int j=0;j<col;j++)
             {
                 mB[i*col+j + r1*(row*col)]=mA[i*col+j + l*(row*col)]+
-                                               mA[(vv1[i]+1)*col+(vv2[j]+1) + l*(row*col)];
+                                               mA[vv1[i]*col+vv2[j] + l*(row*col)];
                 mB[i*col+j + r2*(row*col)]=mA[i*col+j + l*(row*col)]+
-                                               mA[(vv3[i]+1)*col+(vv4[j]+1) + l*(row*col)];
+                                               mA[vv3[i]*col+vv4[j] + l*(row*col)];
             }
         }
     }
@@ -106,8 +107,8 @@ uchar* fullHoughTransformAsym(uchar* image, int row,int col, int r0)
 {
     int r=1;
 
-    uchar v1[ROW_MAX];//можно улучшить
-    uchar v2[COL_MAX];
+    int v1[ROW_MAX];//можно улучшить
+    int v2[COL_MAX];
     uchar index[8*R_MAX];
     uchar di[8*R_MAX+2];
     //!  инициализация массивов
@@ -119,25 +120,24 @@ uchar* fullHoughTransformAsym(uchar* image, int row,int col, int r0)
     int size=col*row;
 
     //! содание массива B
-    uchar* mB=new uchar[size*r0];
-    memset((void*)mB,0,sizeof(uchar)*size*r0);
+    uchar* mB=new uchar[size*r0*8];
+    memset((void*)mB,0,sizeof(uchar)*size*r0*8);
     //! создание массива A
     uchar* mA=new uchar[size*r0*8];
-    memset((void*)mA,0,sizeof(uchar)*size*(r0<<3));
+    memset((void*)mA,0,sizeof(uchar)*size*r0*8);
 
     //! заполнение массива A копиями из исходного изображения
-    for(int i=0;i<(r0<<3);i++)
+    for(int i=0;i<8*r0;i++)
     {
         copyPartImage(mA+i*(size),image,size);
     }
     int colIndex,rowIndex;
 
-    //createIndex(index,5,colIndex,rowIndex);
-    //calcDiff(di,index,colIndex,rowIndex);
+
 
     uchar *x1=mB,*x2=mA,*x3=0;
     while(r<r0)
-    {
+    {        
         x3=x1;x1=x2;x2=x3;
         createIndex(index,r,colIndex,rowIndex);
         calcDiff(di,index,colIndex,rowIndex);
@@ -145,6 +145,7 @@ uchar* fullHoughTransformAsym(uchar* image, int row,int col, int r0)
 
         r=r<<1;
     }
+
 
     return x1;
 }
--- qt/qt_hough/main.cpp
+++ qt/qt_hough/main.cpp
@@ -1,10 +1,15 @@
 #include <QCoreApplication>
+#include <QTime>
 #include "hough_c.h"
+#include "stdio.h"
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
+    QTime t1;t1.start();
     uchar *image=new uchar[1024*768];
-    uchar* result= fullHoughTransformAsym(image,768,1024, 2);
+    uchar* result= fullHoughTransformAsym(image,40,40, 2);
+    QTime t2;t2.msecsTo(t1);
+    printf("t=%f\n",t2.msecsTo(t1));
     return a.exec();
 }
